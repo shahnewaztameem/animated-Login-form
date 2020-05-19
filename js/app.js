0cc3abf12ca22a0d8e352c29085b78ae328c1083
shahnewaz tameem <shahnewaztamim@gmail.com>
2020-05-20 03:48:02 +0600

    Add media queries

--- js/app.js
+++ js/app.js
@@ -16,3 +16,12 @@ function removeActivePasswordField(){
     password.type = 'password';
 }
 
+
+document.getElementById('change-theme-btn').addEventListener('click', function () {
+    let darkThemeEnabled = document.body.classList.toggle('dark-theme');
+    localStorage.setItem('dark-theme-enabled', darkThemeEnabled);
+});
+
+if (JSON.parse(localStorage.getItem('dark-theme-enabled'))) {
+    document.body.classList.add('dark-theme');
+}
